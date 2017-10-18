603f4bb8f1e7e3f260a5559adac111cd29200ecf
Richard Montgomery <rikermonty@gmail.com>
2017-10-17 22:35:10 -0700

    cupcake

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
 'use strict';
 
-var kids = prompt('How many kids?');
-console.log('Number of kids:', kids);
+alert('WoooOOOooOOOoo, Guessing Game Time! \nHow well do you know This Guy?! \n\nThe only site that knows YOU CARE!');
+alert('You will be given 5 questions about me to determine whether we should be friends. \n\n Please answer with a "yes" or "y" or with a "no" or "n"');
 
-var hats = prompt('How many hats?');
-console.log('This many hats:', hats);
+var one = prompt('Given the option of mutant abilities, would I want to be able to fly?');
+console.log('Question One Answer:', one);
 
-if(kids === '3' || kids === '4') {
-  alert('You\'re right!');
+one = one.toLowerCase();
+
+if(one === 'yes' || one === 'y') {
+  alert('No! I have a healthy fear of heights.');
+} else if(one === 'no' || one === 'n'){
+  alert('That\'s right! Flying is for squirrels and fish.');
 };
 
-if(kids === '3' && hats === '2') {
-  alert('You\'re right again!');
+var two = prompt('Am I tired?');
+console.log('Question Two Answer:', two);
+
+two = two.toLowerCase();
+
+if(two === 'yes' || two === 'y') {
+  alert('Yes! I\'m always tired. I take a car/boat/bus-ride to class and back every day!');
+} else if(two === 'no' || two === 'n'){
+  alert('That\'s incorrect. I\'m so tired. I survive on thirty 10 second power naps every day.');
+};
+
+var three = prompt('Are lagers my favorite beers?');
+console.log('Question Three Answer:', three);
+
+three = three.toLowerCase();
+
+if(three === 'yes' || three === 'y') {
+  alert('Nope. IPAs are the right PAs.');
+} else if(three === 'no' || three === 'n'){
+  alert('That\'s right! IPAs for me.');
+};
+
+var four = prompt('Should they reboot the tv series Firefly?');
+console.log('Question Four Answer:', four);
+
+four = four.toLowerCase();
+
+if(four === 'yes' || four === 'y') {
+  alert('YES! RIGHT?!');
+} else if(four === 'no' || four === 'n'){
+  alert('Who hurt you?');
+};
+
+var five = prompt('Is money the root of my happiness?');
+console.log('Question Five Answer:', five);
+
+five = five.toLowerCase();
+
+if(five === 'yes' || five === 'y') {
+  alert('Oh, for sho.');
+} else if(five === 'no' || five === 'n'){
+  alert('Don\'t tell my lady, but yes, money is where it\'s at.');
 };
